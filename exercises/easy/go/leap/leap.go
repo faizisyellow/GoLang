e949ab94b35f19cbc3b7b128423a253143f28f63
faizisyellow <muhamatfaissalm@gmail.com>
2025-02-06 13:21:50 +0700

    exercise: leap [conditional, bolean]

--- exercises/easy/go/leap/leap.go
+++ exercises/easy/go/leap/leap.go
@@ -7,10 +7,9 @@ package leap
 
 // IsLeapYear should have a comment documenting it.
 func IsLeapYear(year int) bool {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	if year%4 == 0 && year%100 != 0 || year%100 == 0 && year%400 == 0 {
+		return true
+	}
 
-	panic("Please implement the IsLeapYear function")
+	return false
 }
