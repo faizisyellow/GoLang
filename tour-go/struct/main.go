67e9f55ddc289b819aa5d01ac8f8f6a7c742eb4f
faizisyellow <muhamatfaissalm@gmail.com>
2025-02-24 19:08:38 +0700

    embed 2 struct

--- tour-go/struct/main.go
+++ tour-go/struct/main.go
@@ -2,18 +2,28 @@ package main
 
 import "fmt"
 
+type Address struct {
+	street, city string
+}
+
 type Greeting struct {
+	Address
 	Name, Feedback string
 }
 
-func New(name, feedback string) *Greeting {
-	new := Greeting{Name: name, Feedback: feedback}
+func New(name, feedback, address, city string) *Greeting {
+	new := Greeting{Name: name, Feedback: feedback,
+		Address: Address{
+			street: address,
+			city:   city,
+		},
+	}
 
 	return &new
 }
 
 func main() {
-	helloTo := New("Faissal", "welcome")
+	helloTo := New("Faissal", "welcome", "10th lincoln", "new york")
 
 	// copied (directly store the value)
 	sayAgain := *helloTo
@@ -22,5 +32,5 @@ func main() {
 
 	fmt.Printf("%v %v\n", helloTo.Feedback, helloTo.Name)
 	fmt.Printf("%v %v\n", sayAgain.Feedback, sayAgain.Name)
-
+	fmt.Println((*helloTo).Address.city)
 }
